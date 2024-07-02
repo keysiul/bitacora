f1e912c36c14b50b686f005b97b34346c63b2fed
keysiul <key.siul@gmail.com>
2024-07-01 23:02:12 -0600

    Inicio

---
+++ vue-bitacora/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+//import Vue from 'vue'
+import App from './App.vue'
+createApp(App).mount('#app');
+
+//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+//Vue.use(BootstrapVue)
+//Vue.use(IconsPlugin)
+
